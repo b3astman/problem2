7a6b8dc4555de519e552e51d7689a89e037ec712
hunterwb <hunterwb@washington.edu>
2016-04-18 18:51:40 -0700

    idl update

--- Round_Robin.c
+++ Round_Robin.c
@@ -1,6 +1,7 @@
 #include "queue.h"
 #include "PCB.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 #define IDL_PID 0xFFFFFFFF
 
@@ -13,7 +14,11 @@ PCB_p currentPCB;
 PCB_p idl;
 
 void dispatcher() {
-	currentPCB = dequeue(readyQueue);
+	if (!isEmpty(readyQueue)) {
+		currentPCB = dequeue(readyQueue);
+	} else {
+		currentPCB = idl;
+	}
 	PCB_set_state(currentPCB, running);
 	sysStack = PCB_get_pc(currentPCB);
 }
@@ -26,7 +31,9 @@ void scheduler(int interruptType) {
 	}
 	if (interruptType == 1) {
 		PCB_set_state(currentPCB, ready);
-		enqueue(readyQueue, currentPCB);
+		if (PCB_get_pid(currentPCB) != IDL_PID) {
+			enqueue(readyQueue, currentPCB);
+		}
 	} 
 	dispatcher();
 }
@@ -43,13 +50,14 @@ int main() {
 	idl = PCB_construct();
 	PCB_init(idl);
 	PCB_set_pid(idl, IDL_PID);
+	currentPCB = idl;
 
 	createdQueue = queue_construct();
 	queue_init(createdQueue);
 	readyQueue = queue_construct();
 	queue_init(readyQueue);
 	
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < 6; i++) {
 
 		int newProcesses = rand() % 6;
 		for (int j = 0; j < newProcesses; j++) {
@@ -57,9 +65,13 @@ int main() {
 			PCB_init(p);
 			PCB_set_pid(p, rand());
 			enqueue(createdQueue, p);
+			printf("Added to ready queue:");
+			PCB_toString(p);
 		}
 
-		sysStack = PCB_get_pc(currentPCB) + rand() % 1001 + 3000;
+		if (PCB_get_pid(currentPCB) != IDL_PID) {
+			sysStack += rand() % 1001 + 3000;
+		}
 		isrTimer();
 
 
